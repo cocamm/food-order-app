cd3cf690e7e90a322a45ec5221e92fbfa7aebcac
Fabio Cunha <fabio@liuv.it>
2022-01-15 17:38:24 -0300

    Added cart modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import HeaderCartButton from "./HeaderCartButton";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="ReactMeals" />
