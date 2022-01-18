02d4fbbbe9a4d788f3201dcaf6ce978ace524904
Fabio Cunha <fabio@liuv.it>
2022-01-18 11:07:47 -0300

    Adusting update itens

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import Modal from "../UI/Modal";
-import CartContext from '../../store/cart-context';
+import CartContext from "../../store/cart-context";
 
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
@@ -12,14 +12,21 @@ const Cart = (props) => {
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = id => {};
+  const cartItemRemoveHandler = (id) => {};
 
-  const cartItemAddHandler = item => {};
+  const cartItemAddHandler = (item) => {};
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price}/>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
